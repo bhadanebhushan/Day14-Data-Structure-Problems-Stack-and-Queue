859acd6489879cb0f9c88dcc5840527b031cd457
bhadanebhushan <bhadanebhushan97@gmail.com>
2022-04-09 22:28:07 +0530

    Ability to create stack of 56->30->70->

--- src/com/stackqueue/Main.java
+++ src/com/stackqueue/Main.java
@@ -2,6 +2,12 @@ package com.stackqueue;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("welcome to stack queue problem ");
+		System.out.println("Welcome to Stack and queue");
+		Stack stack = new Stack();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.displayStack();
+
 	}
 }
